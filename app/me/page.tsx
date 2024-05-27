6d4dc3ec258d76b420d2187141193adf869683e2
grubengraeber <fabiotietz0@gmail.com>
2024-05-28 00:38:06 +0200

    fix for failed actions build

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,12 +1,10 @@
-"use client"
 
 import { Card } from "@/components/ui/card";
 import CV from "./cv";
 
 export default function Me() {
-    const oneThirdScreenHeight = window.innerHeight * 0.33;
 
-    return <div className="flex items-center justify-between" style={{ height: oneThirdScreenHeight }}>
+    return <div className="flex items-center justify-between">
         <div className='p-4 '>
 
             <h1 className='text-black'>Me</h1>
