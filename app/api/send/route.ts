233b300ba115e3e68750532ddd9feba491657e0a
grubengraeber <fabiotietz0@gmail.com>
2024-06-23 16:08:52 +0200

    update

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,11 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { EmailTemplate } from '../../components/email-template';
 import { Resend } from 'resend';
-import { NextRequest, NextResponse } from 'next/server';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: Request) {
       const { name, email, message } = await req.json();
   
 
