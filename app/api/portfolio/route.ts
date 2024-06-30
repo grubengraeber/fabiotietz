712ee5ad1992d2e08f28c74a23905a2a3c79e2d9
grubengraeber <fabiotietz0@gmail.com>
2024-06-30 22:31:57 +0200

    updated with empty response for vercel bug fixing

--- app/api/portfolio/route.ts
+++ app/api/portfolio/route.ts
@@ -1,6 +1,7 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     // TODO: GET Portfolio from Server
     // TODO: Return Portfolio
+    return NextResponse.json({})
 }
