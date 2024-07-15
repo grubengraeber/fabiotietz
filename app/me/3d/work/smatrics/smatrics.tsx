1a469923e7ba89ae93f35bab394ffa63e7b0a418
grubengraeber <fabiotietz0@gmail.com>
2024-07-15 20:23:10 +0200

    renamed

--- app/me/3d/work/smatrics/smatrics.tsx
+++ app/me/3d/work/smatrics/smatrics.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import MeshComponent from "../mesh";
+import MeshComponent from "../../mesh";
 import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
 
 
-export function Avatar() {
+export function Smatrics() {
 
     return (
       <div className='flex justify-center items-center h-screen'>
         <Canvas className='h-2xl w-2xl'>
-        <ambientLight />
         <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={3} />
           <MeshComponent />
           <OrbitControls />
         </Canvas>
