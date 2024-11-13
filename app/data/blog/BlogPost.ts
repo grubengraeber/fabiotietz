a4a27f76a76bf9aab0d18a010d890086286a731d
grubengraeber <fabiotietz0@gmail.com>
2024-11-13 08:15:42 +0100

    refactor: Add BlogPost class for representing blog posts

---
+++ app/data/blog/BlogPost.ts
@@ -0,0 +1,37 @@
+import Technology from "../technology/technology";
+
+class Blog {
+    slug: string;
+    date: Date;
+    title: string;
+    content: string;
+    bannerImage: string;
+    video?: string;
+    sources?: string[];
+    technologies?: Technology[];
+    projectName?: string;
+
+    constructor({ slug, date, title, content, bannerImage, video, sources, technologies, projectName }:{
+        slug: string, 
+        date: Date,
+        title: string, 
+        content: string, 
+        bannerImage: string, 
+        video?: string, 
+        sources?: string[], 
+        technologies?: Technology[],
+        projectName?: string
+    }) {
+        this.slug = slug;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+        this.bannerImage = bannerImage;
+        this.video = video;
+        this.sources = sources;
+        this.technologies = technologies;
+        this.projectName = projectName;
+    }
+}
+
+export default Blog;
