18c8c6d7fb1f6a4c257cbceafe84606c756bc24a
grubengraeber <fabiotietz0@gmail.com>
2024-11-13 08:14:36 +0100

    refactor: Add API route for fetching blogs

---
+++ app/api/blogs/[slug]/route.ts
@@ -0,0 +1,24 @@
+import BlogService from "@/app/service/BlogService";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, { params} : { params: { slug: string } }) {
+    const slug = params.slug;
+
+    if (!slug) {
+        console.log("Blog not found");
+        return NextResponse.json({
+            data: null,
+            error: "Blog not found"
+        })
+    }
+
+    const blogService = new BlogService();
+
+
+    const project = blogService.getBlog(slug);
+
+    return NextResponse.json({
+        data: project,
+        error: project ? null : "Blog not found"
+    })
+}
