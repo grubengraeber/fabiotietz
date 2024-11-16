9c6a32d6a8f4a11d57f0a7cfcee8a0b0e66f003a
grubengraeber <fabiotietz0@gmail.com>
2024-11-16 03:10:24 +0100

    chore: Update projects-detail-view component with ReactMarkdown for rendering project content

--- app/components/projects/projects-detail-view.tsx
+++ app/components/projects/projects-detail-view.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import { useTheme } from "next-themes";
 import { Badge } from "@/components/ui/badge";
 import ColorService from "@/app/service/ColorService";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import "@/css/github-markdown.css"
 
 const ProjectDetailView: React.FC<{project: Project}> = ({project}: {project: Project}) => {
   const theme = useTheme();
@@ -15,12 +19,20 @@ const ProjectDetailView: React.FC<{project: Project}> = ({project}: {project: Pr
     <div className="container mx-auto p-4">
       <div className="grid lg:grid-cols-2 max-md:grid-cols-1">
       <Image src={isDarkMode && project.bannerDark ? project.bannerDark : project.bannerLight} alt={project.title} className="w-full object-cover rounded-lg" height={50} width={100} />
-      <div className="text-center">
-        <div className="flex-row gap-5">
-          <h1 className="lg:text-8xl max-md:text-3xl font-bold mt-4 text-center">{project.title}</h1>
-          <Badge className={`${colorService.backgroundForStatus(project)} ${colorService.textForStatus(project)}`} >{project.status}</Badge>
+      <div className="">
+        <div className="flex-row gap-5 text-center">
+          <h1 className="lg:text-8xl max-md:text-3xl font-bold mt-4">{project.title}</h1>
+          <Badge className={`${colorService.backgroundForStatus(project)} ${colorService.textForStatus(project)} mt-4`} >{project.status}</Badge>
         </div>
-      <p className="mt-2">{project.content}</p>
+      <div className="m-4">
+      <ReactMarkdown
+      className={"markdown-body"}
+      remarkPlugins={[remarkGfm]}
+      rehypePlugins={[rehypeRaw]}
+      >
+        {project.content}
+      </ReactMarkdown>
+      </div>
       {project.blogPost && (
         <Link href={project.blogPost} className="text-blue-500 hover:underline">
           Blog Post
