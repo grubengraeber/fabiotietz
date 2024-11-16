7107df25d524b4559bd59ac1216146c882b1a118
grubengraeber <fabiotietz0@gmail.com>
2024-11-16 02:30:48 +0100

    feat: Add tags to BlogPreview component

--- app/blog/BlogPreview.tsx
+++ app/blog/BlogPreview.tsx
@@ -1,21 +1,30 @@
 import Image from "next/image"
 import Link from "next/link"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 
-export default function BlogPreview({ title, imageUrl, slug }: { title: string, imageUrl: string, slug: string }) {
+export default function BlogPreview({ title, imageUrl, slug, tags }: { title: string, imageUrl: string, slug: string, tags?: string[] }) {
   return (
     <Link href={`/blog/${slug}`} className="group">
       <Card className="overflow-hidden transition-shadow hover:shadow-lg">
         <div className="relative aspect-video">
           <Image
             src={imageUrl}
-            alt=""
+            alt="The Banner image of the blog post"
             fill
             className="object-cover transition-transform group-hover:scale-105"
           />
         </div>
         <CardContent className="p-4">
           <h2 className="text-2xl font-bold line-clamp-2 group-hover:underline">{title}</h2>
+          <CardFooter className="mt-4">
+            <div className="flex flex-wrap gap-2">
+              {
+                tags?.map(tag => (
+                  <span key={tag} className="text-sm text-gray-500">#{tag}</span>
+                ))
+              }
+            </div>
+          </CardFooter>
         </CardContent>
       </Card>
     </Link>
